c87bac86c0bd46a2056041aa5e88822376e6725f
Marconi Gomes <mgrf@cin.ufpe.br>
2020-10-29 18:42:17 -0300

    Implementando rota 'direito de voto sobre a fila' no servidor.

--- rusbe-server/src/repositories/queue.repository.ts
+++ rusbe-server/src/repositories/queue.repository.ts
@@ -1,1 +1,32 @@
-export class QueueRepository{ }
+import UserSchema from "../models/user";
+import User from "../../../common/user";
+
+export class QueueRepository{
+    /**
+     * Gets right to vote for the queue size by the given userId.
+     * @param userId The ID of the user to search the right to vote.
+     * @returns A boolean, true if the user has the right to vote, false otherwise.
+     */
+    async canVote(userId): Promise<boolean> {
+        try {
+            const user = await this.getUserInfo(userId);
+            return user.voteRight;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+
+    /**
+     * Gets the User by its id and returns it.
+     * @param id The ID of the user to be searched.
+     * @returns A user if found.
+     */
+    async getUserInfo(id): Promise<User> {
+        try {
+            const user = await UserSchema.findOne({id});
+            return user as unknown as User;
+        } catch (error) {
+            throw new Error('User not found!')
+        }
+    }
+}
--- rusbe-server/src/services/queue.service.ts
+++ rusbe-server/src/services/queue.service.ts
@@ -1,9 +1,17 @@
 import Router from 'express'
+import { QueueRepository } from "../repositories/queue.repository";
 
-const queueService = Router()
+const queueService = Router();
+const queueRepository = new QueueRepository();
+const queueBasePath = '/queue';
 
-queueService.get('/queue',async(req,res)=>{
-    res.send({msg:"Olá"})
+queueService.get(queueBasePath + '/voteRight',async(req,res)=>{
+    try {
+        const canVote = await queueRepository.canVote(req.body.userId);
+        res.send(canVote);
+    } catch (error) {
+        res.status(404).send({message: error.message});
+    }
 })
 
 export default queueService
