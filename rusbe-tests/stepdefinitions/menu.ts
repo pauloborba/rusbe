3a6e7b938428e6fc7e9fd9808efa869e97cc0412
Andre Luis <alpvj@cin.ufpe.br>
2020-11-12 18:30:28 -0300

    Modularizando testes de aceitação

--- rusbe-tests/stepdefinitions/menu.ts
+++ rusbe-tests/stepdefinitions/menu.ts
@@ -3,30 +3,45 @@ import { browser, $, element, ElementArrayFinder, by } from 'protractor';
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 
+let currentUser = async () => {
+    var data = <string> await browser.executeScript('return window.localStorage.getItem("user");');
+    return (data!=null&&data!=="") ? JSON.parse(data) : {id:"", optionsVoted:[]};
+};
+
+let currentUserId = async () => {
+    return (await currentUser()).id||"";
+};
+
+let currentUserOptionsVoted = async () => {
+    return (await currentUser()).optionsVoted||[];
+};
+
+let checkElementInArray = <bool> (key:string, arr:string|[string]) => {
+    return arr.indexOf(key)!=-1;;
+};
+
+let goToPage = async (url) => {
+    await browser.get(url);
+};
 
 defineSupportCode(function ({ Given, When, Then }) {
 
     Given(/^I am at the "([^\"]*)" page$/, async (pageName : string) => {
-        //TODO: Botar isso no before all
         const url = "http://localhost:4200/screens/"+pageName.toLowerCase();
-        await browser.get(url);
-        await browser.sleep(1500);//Precisa fazer requisição das comidas pelo MongoDB
-        //ate aqui
+        await goToPage(url);
+        await browser.sleep(1500);//Espera pelo get do MongoDB
         await expect(browser.getCurrentUrl()).to.eventually.equal(url);
     });
 
     Given(/^I am logged in to "([^\"]*)" user account$/, async (login : string) => {
-        //TODO: Botar isso no before all
-        const urlLogin = "http://localhost:4200/screens/login";
-        await browser.get(urlLogin);
-        await expect (browser.getCurrentUrl()).to.eventually.equal(urlLogin);
+        const url = "http://localhost:4200/screens/login";
+        await goToPage(url);
+        await expect (browser.getCurrentUrl()).to.eventually.equal(url);
         await element(by.name('uname')).sendKeys(<string> login);
         await element(by.name('form')).submit();
-        await browser.sleep(1500);//Precisa fazer autenticação pelo MongoDB
-        //ate aqui
-        var data = <string> await browser.executeScript('return window.localStorage.getItem("user");');
-        data = (data!=null&&data!=="") ? JSON.parse(data).id : "";
-        await expect(data).equal(login);
+        await browser.sleep(2000);//Precisa fazer autenticação pelo MongoDB
+        var userId : string = await currentUserId();
+        await expect(userId).equal(login);
     });
 
     Given(/^I can see "([^\"]*)" as the current meal for the menu$/, async (meal) => {
@@ -36,14 +51,11 @@ defineSupportCode(function ({ Given, When, Then }) {
 
     Then(/^I can see "([^\"]*)", "([^\"]*)", "([^\"]*)", "([^\"]*)" listed$/, async (food0:string,food1:string,food2:string,food3:string) => {
         const foodOptions = await element.all(by.name('foodname')).getText();
-        const booleanArray = [
-            foodOptions.indexOf(food0)!=-1,
-            foodOptions.indexOf(food1)!=-1,
-            foodOptions.indexOf(food2)!=-1,
-            foodOptions.indexOf(food3)!=-1
-        ];
-        var allTrue : boolean = true;
-        for (let d of booleanArray) allTrue = allTrue&&d;
+        var allTrue : boolean = 
+            checkElementInArray(food0, foodOptions) &&
+            checkElementInArray(food1, foodOptions) &&
+            checkElementInArray(food2, foodOptions) &&
+            checkElementInArray(food3, foodOptions);
         await expect(allTrue).equal(true);
     });
 
@@ -53,14 +65,13 @@ defineSupportCode(function ({ Given, When, Then }) {
     
     Given(/^I can see "([^\"]*)" at the menu$/, async (food:string) => {
         const foodOptions = await element.all(by.name('foodname')).getText();
-        const foundFood = foodOptions.indexOf(food)!=-1;
+        const foundFood = checkElementInArray(food, foodOptions);
         await expect(foundFood).equal(true);
     });
 
     Given(/^I have not voted for "([^\"]*)"$/, async (food:string) => {
-        var votedFood = <string> await browser.executeScript('return window.localStorage.getItem("user");');
-        votedFood = (votedFood!=null&&votedFood!=="") ? JSON.parse(votedFood).optionsVoted : [];
-        const foundFood = votedFood.indexOf(food)!=-1;
+        const votedFood = await currentUserOptionsVoted();
+        const foundFood = checkElementInArray(food, votedFood);
         await expect(foundFood).equal(false);
     });
 
@@ -70,10 +81,8 @@ defineSupportCode(function ({ Given, When, Then }) {
     });
 
     Then(/^"([^\"]*)" is at the list of evaluated foods$/, async (food:string) => {
-        var votedFood = <string> await browser.executeScript('return window.localStorage.getItem("user");');
-        votedFood = (votedFood!=null&&votedFood!=="") ? JSON.parse(votedFood).optionsVoted : [];
-        console.log(votedFood);
-        const foundFood = votedFood.indexOf(food)!=-1;
+        const votedFood = await currentUserOptionsVoted();
+        const foundFood = checkElementInArray(food, votedFood);
         await expect(foundFood).equal(true);
     });
 
