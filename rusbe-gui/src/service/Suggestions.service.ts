3d8e56c63117a65851d3861bccac7a0d1c1a5273
Erick Almeida <erickingles@gmail.com>
2020-11-16 17:56:16 -0300

    Implementa o serviço de sugestões

--- rusbe-gui/src/service/Suggestions.service.ts
+++ rusbe-gui/src/service/Suggestions.service.ts
@@ -1,9 +1,22 @@
-import { Injectable }    from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import Suggestion from '../../../common/suggestion';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class SuggestionsService {
-  private headers = new HttpHeaders({'Content-Type': 'application/json'});
-  private apiURL = 'http://localhost:3000';
+  private readonly headers = new HttpHeaders({'Content-Type': 'application/json'});
+  private readonly apiURL = 'http://localhost:3333/suggestions';
+
+  constructor(public http: HttpClient) { }
+
+  getUserSuggestions(userId): Observable<any> {
+    return this.http.get(`${this.apiURL}?userId=${userId}`, {headers: this.headers});
+  }
+
+  newSuggestion(suggestion: Suggestion): Observable<any> {
+    return this.http.post(this.apiURL + '/new', suggestion, {headers: this.headers});
+  }
 }
